d53128ca1036930bdda3adf7123872a96290f252
unknown <robert.ross.iv@icloud.com>
2017-10-14 14:07:22 -0400

    initial commit

---
+++ PackGenCsharp/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace PackGenCsharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPack = new System.Windows.Forms.TextBox();
+            this.btnPick = new System.Windows.Forms.Button();
+            this.btnGen = new System.Windows.Forms.Button();
+            this.lbPossibles = new System.Windows.Forms.ListBox();
+            this.lbDraws = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPack
+            // 
+            this.tbPack.Location = new System.Drawing.Point(372, 11);
+            this.tbPack.Name = "tbPack";
+            this.tbPack.Size = new System.Drawing.Size(37, 20);
+            this.tbPack.TabIndex = 0;
+            this.tbPack.TextChanged += new System.EventHandler(this.tbPack_TextChanged);
+            // 
+            // btnPick
+            // 
+            this.btnPick.Location = new System.Drawing.Point(148, 10);
+            this.btnPick.Name = "btnPick";
+            this.btnPick.Size = new System.Drawing.Size(75, 23);
+            this.btnPick.TabIndex = 1;
+            this.btnPick.Text = "Pick Deck";
+            this.btnPick.UseVisualStyleBackColor = true;
+            this.btnPick.Click += new System.EventHandler(this.btnPick_Click);
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(413, 10);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(75, 23);
+            this.btnGen.TabIndex = 4;
+            this.btnGen.Text = "Open Packs";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // lbPossibles
+            // 
+            this.lbPossibles.FormattingEnabled = true;
+            this.lbPossibles.Location = new System.Drawing.Point(12, 58);
+            this.lbPossibles.Name = "lbPossibles";
+            this.lbPossibles.Size = new System.Drawing.Size(211, 251);
+            this.lbPossibles.TabIndex = 5;
+            // 
+            // lbDraws
+            // 
+            this.lbDraws.FormattingEnabled = true;
+            this.lbDraws.Location = new System.Drawing.Point(277, 58);
+            this.lbDraws.Name = "lbDraws";
+            this.lbDraws.Size = new System.Drawing.Size(211, 251);
+            this.lbDraws.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number of Packs:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Possible Cards:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(274, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Opened Packs:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 314);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbDraws);
+            this.Controls.Add(this.lbPossibles);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.btnPick);
+            this.Controls.Add(this.tbPack);
+            this.Name = "Form1";
+            this.Text = "Legends Of Today (Pack Opener)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPack;
+        private System.Windows.Forms.Button btnPick;
+        private System.Windows.Forms.Button btnGen;
+        private System.Windows.Forms.ListBox lbPossibles;
+        private System.Windows.Forms.ListBox lbDraws;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PackGenCsharp/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PackGenCsharp
+{
+    public partial class Form1 : Form
+    {
+        //Open File dialog
+        //plug in text file
+        //put text file in text box
+        //get number from num iterations
+        //for loop for number of iterations
+
+        //integer for the desired number of opened card packs
+        //array to hold every possible card
+        int numPacks; string[] cards; string chosenDeck;
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPick_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Filter = "Text (*.txt)|*.txt";
+            if (System.Windows.Forms.DialogResult.OK == openfile.ShowDialog())
+            {
+                string myfile = openfile.FileName;
+                chosenDeck = myfile;
+                cards = File.ReadAllLines(myfile);
+
+                //for each loop to put every card in the left list box to show possibles
+                foreach (string item in cards)
+                {
+                    lbPossibles.Items.Add(item);
+                }                          
+
+            }
+        }
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            //Checking if number of packs is empty
+            if (tbPack.Text != "")
+            {
+                numPacks = int.Parse(tbPack.Text);
+                Random r = new Random();
+
+                //Checking if number of packs is not zero or a negative number
+                if (numPacks >= 1)
+                {
+                    //Checking if deck file is selected
+                    if (cards != null)
+                    {
+                        //for loop to run for every pack
+                        for (int i = 0; i < numPacks; i++)
+                        {
+                            //for loop to randomly draw five cards for every pack
+                            for (int j = 0; j < 5; j++)
+                            {
+                                lbDraws.Items.Add(cards[r.Next(cards.Length)]);
+                            }
+                            //add a space to seperate packs
+                            lbDraws.Items.Add("");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("You must select a valid deck file!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("You must type a positive number of card packs, at least one or more!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must select a number of card packs to open!");
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbPack_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+ 
